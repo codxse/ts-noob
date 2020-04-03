2e63a6e0a63ef1d69335f4c7adaacfd39fe11829
Nadiar AS <codxse@gmail.com>
2020-04-03 23:18:10 +0700

    mymap

--- design-pattren/maps/src/index.ts
+++ design-pattren/maps/src/index.ts
@@ -1,26 +1,37 @@
-import { User } from "./model/User";
-import faker from "faker";
-import { Location } from "./model/Location";
-import { Company } from "./model/Company";
+/// <reference types="@types/googlemaps" />
+import { MyMap } from "./MyMap";
 
-const user1 : User = new User(
-  faker.name.firstName(),
-  new Location(
-    parseFloat(faker.address.longitude()),
-    parseFloat(faker.address.latitude())
-  )
-);
+// import { User } from "./model/User";
+// import faker from "faker";
+// import { Location } from "./model/Location";
+// import { Company } from "./model/Company";
 
-const company1 : Company = new Company(
-  faker.company.companyName(),
-  faker.company.catchPhrase(),
-  new Location(
-    parseFloat(faker.address.longitude()),
-    parseFloat(faker.address.latitude())
-  )
-);
+// const user1 : User = new User(
+//   faker.name.firstName(),
+//   new Location(
+//     parseFloat(faker.address.longitude()),
+//     parseFloat(faker.address.latitude())
+//   )
+// );
 
+// const company1 : Company = new Company(
+//   faker.company.companyName(),
+//   faker.company.catchPhrase(),
+//   new Location(
+//     parseFloat(faker.address.longitude()),
+//     parseFloat(faker.address.latitude())
+//   )
+// );
 
+// const mapEl : Element = document.getElementById('map');
+// const mapOpt : google.maps.MapOptions = {
+//   zoom: 1,
+//   center: {
+//     lat: parseFloat(faker.address.latitude()),
+//     lng: parseFloat(faker.address.longitude()),
+//   }
+// };
 
-// console.log(user1);
-// console.log(company1);
+// new google.maps.Map(mapEl, mapOpt);
+
+new MyMap();
