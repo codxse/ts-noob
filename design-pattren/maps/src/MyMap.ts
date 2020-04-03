e715ca2998ecca0f11aad6009f47b990cd0b6316
Nadiar AS <codxse@gmail.com>
2020-04-03 23:28:40 +0700

    bad code

--- design-pattren/maps/src/MyMap.ts
+++ design-pattren/maps/src/MyMap.ts
@@ -1,8 +1,11 @@
+import { Company } from "./model/Company";
+import { User } from "./model/User";
+
 export class MyMap {
-  private googleMap: google.maps.Map;
+  private _googleMap: google.maps.Map;
 
   constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+    this._googleMap = new google.maps.Map(document.getElementById('map'), {
       zoom: 1,
       center: {
         lat: 0,
@@ -10,4 +13,25 @@ export class MyMap {
       }
     })
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this._googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      }      
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this._googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      }      
+    });
+  }
+
 }
--- design-pattren/maps/src/index.ts
+++ design-pattren/maps/src/index.ts
@@ -1,27 +1,27 @@
 /// <reference types="@types/googlemaps" />
 import { MyMap } from "./MyMap";
 
-// import { User } from "./model/User";
-// import faker from "faker";
-// import { Location } from "./model/Location";
-// import { Company } from "./model/Company";
+import { User } from "./model/User";
+import faker from "faker";
+import { Location } from "./model/Location";
+import { Company } from "./model/Company";
 
-// const user1 : User = new User(
-//   faker.name.firstName(),
-//   new Location(
-//     parseFloat(faker.address.longitude()),
-//     parseFloat(faker.address.latitude())
-//   )
-// );
+const user1 : User = new User(
+  faker.name.firstName(),
+  new Location(
+    parseFloat(faker.address.longitude()),
+    parseFloat(faker.address.latitude())
+  )
+);
 
-// const company1 : Company = new Company(
-//   faker.company.companyName(),
-//   faker.company.catchPhrase(),
-//   new Location(
-//     parseFloat(faker.address.longitude()),
-//     parseFloat(faker.address.latitude())
-//   )
-// );
+const company1 : Company = new Company(
+  faker.company.companyName(),
+  faker.company.catchPhrase(),
+  new Location(
+    parseFloat(faker.address.longitude()),
+    parseFloat(faker.address.latitude())
+  )
+);
 
 // const mapEl : Element = document.getElementById('map');
 // const mapOpt : google.maps.MapOptions = {
@@ -34,4 +34,7 @@ import { MyMap } from "./MyMap";
 
 // new google.maps.Map(mapEl, mapOpt);
 
-new MyMap();
+const myMap : MyMap = new MyMap();
+
+myMap.addUserMarker(user1);
+myMap.addUserMarker(company1);
