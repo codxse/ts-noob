5722adfaab7327c2a4370d73b7ac93198cbdb1b4
Nadiar AS <codxse@gmail.com>
2020-04-03 22:45:06 +0700

    goog

--- design-pattren/maps/src/index.ts
+++ design-pattren/maps/src/index.ts
@@ -1,24 +1,24 @@
-// import { User } from "./model/User";
-// import faker from "faker";
-// import { Location } from "./model/Location";
-// import { Company } from "./model/Company";
+import { User } from "./model/User";
+import faker from "faker";
+import { Location } from "./model/Location";
+import { Company } from "./model/Company";
 
-// const user1 : User = new User(
-//   faker.name.firstName(),
-//   new Location(
-//     parseFloat(faker.address.longitude()),
-//     parseFloat(faker.address.latitude())
-//   )
-// );
+const user1 : User = new User(
+  faker.name.firstName(),
+  new Location(
+    parseFloat(faker.address.longitude()),
+    parseFloat(faker.address.latitude())
+  )
+);
 
-// const company1 : Company = new Company(
-//   faker.company.companyName(),
-//   faker.company.catchPhrase(),
-//   new Location(
-//     parseFloat(faker.address.longitude()),
-//     parseFloat(faker.address.latitude())
-//   )
-// );
+const company1 : Company = new Company(
+  faker.company.companyName(),
+  faker.company.catchPhrase(),
+  new Location(
+    parseFloat(faker.address.longitude()),
+    parseFloat(faker.address.latitude())
+  )
+);
 
 
 
